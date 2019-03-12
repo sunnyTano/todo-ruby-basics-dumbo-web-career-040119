20df760f1d652b2614d1ea264e6aa7ae85414ee0
Susan Tan <ssusantan9@gmail.com>
2019-03-12 02:34:07 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -14,8 +14,8 @@ def greeting(greeting, name)
   return greeting + name
 end
 
-def return_a_value
-
+def return_a_value(phrase)
+  return phrase
 end
 
 def last_evaluated_value
@@ -27,22 +27,6 @@ def pizza_party
 end
 
 =begin 
-
-  describe '#greeting' do
-    it 'takes two arguments' do
-      greeting = "Hi there, "
-      name = "Bobby!"
-
-      expect{ greeting(greeting, name) }.to_not raise_error
-    end
-
-    it 'raises an error with one argument' do
-      greeting = "Hi there, "
-
-      expect{ greeting(greeting)}.to raise_error
-    end
-  end
-
   describe '#return_a_value' do
     it 'returns the phrase "Nice"' do
       expect(return_a_value).to eq("Nice")
